5719b10861d5f223a976bbb0ff75af726a9f321a
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2024-02-15 10:38:35 -0800

    Intercept port 445
    Rewrite to 4445 outgoing, back to 445 incoming.  This DOES NOT show up under Wireshark,, however.

--- examples/cpp/capture/capture.cpp
+++ examples/cpp/capture/capture.cpp
@@ -4,6 +4,19 @@
 #include "pch.h"
 #include <iostream>
 
+#define ip_size sizeof(struct iphdr)
+#define tcp_size  sizeof(struct tcphdr)
+#define packet buffer.m_IBuffer
+
+#define TCP_PORT_SMB3 0x01BD
+#define TCP_PORT_SMB3_NET 0xBD01
+
+#define TCP_PORT_4445 0x115D
+#define TCP_PORT_4445_NET 0x5D11
+
+#define LOCAL_SUBNET 0xC0A8
+#define LOCAL_SUBNET_NET 0xA8C0
+
 int main()
 {
 	try
@@ -14,16 +27,39 @@ int main()
 		auto ndis_api = std::make_unique<ndisapi::fastio_packet_filter>(
 			[&file_stream](HANDLE, INTERMEDIATE_BUFFER& buffer)
 			{
-				file_stream << buffer;
+                file_stream << buffer;
 
-				return ndisapi::fastio_packet_filter::packet_action::pass;
+                struct ether_header* ethernet = (struct ether_header*)packet;
+                if (ethernet->h_proto == ETH_P_IP_NET) {
+                    struct iphdr* ip = (struct iphdr*)(packet + ETHER_HEADER_LENGTH);
+                    if (ip->ip_p == IPPROTO_TCP && ip->ip_dst.S_un.S_un_w.s_w1 != LOCAL_SUBNET_NET) {
+                        struct tcphdr* tcp = (struct tcphdr*)(packet + ETHER_HEADER_LENGTH + ip_size);
+                        if (tcp->th_sport == TCP_PORT_4445_NET) {
+                            tcp->th_sport = TCP_PORT_SMB3_NET;
+                        }
+                    }
+                }
+
+                return ndisapi::fastio_packet_filter::packet_action::pass;
 			},
 			[&file_stream](HANDLE, INTERMEDIATE_BUFFER& buffer)
 			{
-				file_stream << buffer;
+                struct ether_header* ethernet = (struct ether_header*)packet;
+                if (ethernet->h_proto == ETH_P_IP_NET) {
+                    struct iphdr* ip = (struct iphdr*)(packet + ETHER_HEADER_LENGTH);
+                    if (ip->ip_p == IPPROTO_TCP && ip->ip_dst.S_un.S_un_w.s_w1 != LOCAL_SUBNET_NET) {
+                        struct tcphdr* tcp = (struct tcphdr*)(packet + ETHER_HEADER_LENGTH + ip_size);
+                        if (tcp->th_dport == TCP_PORT_SMB3_NET) {
+                            tcp->th_dport = TCP_PORT_4445_NET;
+                        }
+                    }
+                }
 
-				return ndisapi::fastio_packet_filter::packet_action::pass;
-			}, true);
+                file_stream << buffer;
+
+                return ndisapi::fastio_packet_filter::packet_action::pass;
+			}, 
+            true);
 
 		if (ndis_api->IsDriverLoaded())
 		{
@@ -35,15 +71,20 @@ int main()
 			return 1;
 		}
 
-		std::cout << "Available network interfaces:" << std::endl << std::endl;
-		size_t index = 0;
-		for (auto& e : ndis_api->get_interface_names_list())
-		{
-			std::cout << ++index << ")\t" << e << std::endl;
-		}
+        std::cout << "Available network interfaces:" << std::endl << std::endl;
+        size_t index = 0;
+        for (auto& e : ndis_api->get_interface_names_list())
+        {
+            std::cout << ++index << ")\t" << e << std::endl;
+        }
 
+#if 1
+        index = 7;
+        std::cout << "Listening on: " << ndis_api->get_interface_names_list()[index - 1] << std::endl;
+#else
 		std::cout << std::endl << "Select interface to filter:";
 		std::cin >> index;
+#endif
 
 		if (index > ndis_api->get_interface_names_list().size())
 		{
@@ -51,8 +92,12 @@ int main()
 			return 0;
 		}
 
+#if 1
+        file_name = "d:\\temp\\capture.pcap";
+#else
 		std::cout << std::endl << "Enter filename to save the capture:";
 		std::cin >> file_name;
+#endif
 
 		file_stream.open(file_name);
 
